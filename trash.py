164e83b4a8039a0b419e116d82e1f288f2622fb6
QuangHuy54 <lhbac07@mgmt01>
2024-06-11 14:11:33 +0000

    backup

--- trash.py
+++ trash.py
@@ -12,11 +12,12 @@ for idx1,line1 in enumerate(w_2):
             s = fuzz.ratio(line, line1)
             if s>90:
                 sum+=1
-                print(line)
-                print(line1)
-                print(w_3[idx])
-                print(w_4[idx1])
-                print(s)
+                print(idx1)
+                # print(line)
+                # print(line1)
+                # print(w_3[idx])
+                # print(w_4[idx1])
+                # print(s)
                 delete_one.add(idx1)
                 break
 w_5=open(f'../data_RQ3/train/delete.txt', 'w')
