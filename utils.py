e17d6f3eceb35547058ecd703f4f1ad67cd5891d
QuangHuy54 <lhbac07@mgmt01>
2024-06-12 13:11:51 +0000

    backup

--- utils.py
+++ utils.py
@@ -103,7 +103,7 @@ class Vocab(object):
     def __len__(self):
         return self.num_words
 
-    def load_txt(path):
+    def load_txt(self,path):
         with open(path,'r') as f1:
             for line in f1.readlines():
                 word=line.replace('\n','')
