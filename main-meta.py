28e8d662918324048c029b7bb61cf4f234cb18e9
QuangHuy54 <lhbac07@mgmt01>
2024-06-13 07:17:34 +0000

    backup

--- main-meta.py
+++ main-meta.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
     best_model_dict = _train(training_projects=training_projects, \
                             validating_project=validating_project,\
                             vocab_file_path=(config.code_vocab_path, config.ast_vocab_path, config.nl_vocab_path)
-                            ,model_file_path='./model/pretrain.pt')
+                            ,model_file_path='../pretrain_model/pretrain.pt')
     _test(best_model_dict,vocab_file_path=(config.code_vocab_path, config.ast_vocab_path, config.nl_vocab_path),testing_project=testing_project,num_of_data=10)
     
     #  _test(os.path.join('20240511_132257', 'model_valid-loss-3.3848_epoch-14_batch--1.pt'))
