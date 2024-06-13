5eb4f08711a0fa457d831c4eed3cbf39116469bf
QuangHuy54 <lhbac07@mgmt01>
2024-06-13 15:08:01 +0000

    backup

--- main-meta-test.py
+++ main-meta-test.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
     for file in dir_list:
         print(f'File name: ',file)
         config.logger.info(f'File name: {file}')
-        _test(os.path.join(path,file),vocab_file_path=(config.code_vocab_path, config.ast_vocab_path, config.nl_vocab_path),testing_project=testing_project,num_of_data=10)
+        _test(os.path.join(path,file),vocab_file_path=(config.code_vocab_path, config.ast_vocab_path, config.nl_vocab_path),testing_project=testing_project,num_of_data=100)
     
     #  _test(os.path.join('20240511_132257', 'model_valid-loss-3.3848_epoch-14_batch--1.pt'))
 
