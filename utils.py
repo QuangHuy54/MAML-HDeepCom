fefabd552ab11c803939f49106282bfde6a6f47f
QuangHuy54 <lhbac07@mgmt01>
2024-06-16 14:19:01 +0000

    backup

--- utils.py
+++ utils.py
@@ -523,14 +523,14 @@ def sentence_bleu_score(reference, candidate) -> float:
     smoothing_function = nltk.translate.bleu_score.SmoothingFunction()
     return nltk.translate.bleu_score.sentence_bleu(references=[reference],
                                                    hypothesis=candidate,
-                                                   smoothing_function=None)
+                                                   smoothing_function=smoothing_function.method3)
 
 
 def corpus_bleu_score(references, candidates) -> float:
     smoothing_function = nltk.translate.bleu_score.SmoothingFunction()
     return nltk.translate.bleu_score.corpus_bleu(list_of_references=[[reference] for reference in references],
                                                  hypotheses=[candidate for candidate in candidates],
-                                                 smoothing_function=None)
+                                                 smoothing_function=smoothing_function.method3)
 
 
 def meteor_score(reference, candidate):
