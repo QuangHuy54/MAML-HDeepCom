93426f25936db2727c56818f3fb9379309ef304f
QuangHuy54 <lhbac07@mgmt01>
2024-07-20 17:29:48 +0000

    backup

--- main-baseline-2.py
+++ main-baseline-2.py
@@ -112,7 +112,7 @@ if __name__ == '__main__':
         training_projects=args.train
 
     validating_project=args.validate if args.validate != None else project2validate[testing_project]  
-    best_model_dict = _train(testing_project,is_transfer=False,vocab_file_path=(config.code_vocab_path, config.ast_vocab_path, config.nl_vocab_path),model_file_path='../pretrain_model/pretrain.pt',)
+    best_model_dict = _train(testing_project,is_transfer=False,vocab_file_path=(config.code_vocab_path, config.ast_vocab_path, config.nl_vocab_path),model_file_path='../pretrain_model/pretrain.pt',training_projects=training_projects,validating_project=validating_project)
 
     # best_model_dict2=_train(testing_project,is_transfer=True,vocab_file_path=(config.code_vocab_path, config.ast_vocab_path, config.nl_vocab_path),model_state_dict=best_model_dict,num_of_data=100)
 
