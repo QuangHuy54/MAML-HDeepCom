efdbde9bcc9c53925349ad3db055e7a3f8bd24fa
QuangHuy54 <lhbac07@mgmt01>
2024-07-25 14:42:05 +0000

    backup

--- get-rank-dataset.py
+++ get-rank-dataset.py
@@ -68,48 +68,48 @@ def get_result(project,projects):
         target_ast_semantic=torch.max(target_ast_semantic, 0).values
         rank_code[target_pro]=torch.cosine_similarity(original_code_semantic,target_semantic,-1).item()
         rank_ast[target_pro]=torch.cosine_similarity(original_ast_semantic,target_ast_semantic,-1).item()
-        with open(f'../dataset_v2/original/{target_pro}/all_truncated_final.code',"r") as f2:
-            data_target=f2.readlines()
-        rank_rouge[target_pro]=get_rouge(data_original,data_target)
-        rank_length[target_pro]=abs(get_total_length(data_original)-get_total_length(data_target))
+        # with open(f'../dataset_v2/original/{target_pro}/all_truncated_final.code',"r") as f2:
+        #     data_target=f2.readlines()
+        # rank_rouge[target_pro]=get_rouge(data_original,data_target)
+        # rank_length[target_pro]=abs(get_total_length(data_original)-get_total_length(data_target))
     #print(rank_code)  
     rank_code=dict(sorted(rank_code.items(), key=lambda item: item[1],reverse=True))
     rank_ast=dict(sorted(rank_ast.items(), key=lambda item: item[1],reverse=True))
-    rank_rouge=dict(sorted(rank_rouge.items(), key=lambda item: item[1],reverse=True))
-    rank_length=dict(sorted(rank_length.items(), key=lambda item: item[1]))
+    # rank_rouge=dict(sorted(rank_rouge.items(), key=lambda item: item[1],reverse=True))
+    # rank_length=dict(sorted(rank_length.items(), key=lambda item: item[1]))
     with open(f'../dataset_v2/original/{project}/result_meta_dataset.txt',"w") as f:
         f.write(project+'\n')
         f.flush()
         print(project, flush=True)
         print("Rank code: ",' '.join([x for x in rank_code.keys()]), flush=True)
-        print("Rank rouge: ",' '.join([x for x in rank_rouge.keys()]), flush=True)
-        print("Rank length: ",' '.join([x for x in rank_length.keys()]), flush=True)
+        # print("Rank rouge: ",' '.join([x for x in rank_rouge.keys()]), flush=True)
+        # print("Rank length: ",' '.join([x for x in rank_length.keys()]), flush=True)
         print("Rank ast: ",' '.join([x for x in rank_ast.keys()]), flush=True)
         f.write("Rank code: "+' '.join([x for x in rank_code.keys()])+'\n')
         f.flush()
-        f.write("Rank rouge: "+' '.join([x for x in rank_rouge.keys()])+'\n')
-        f.flush()
-        f.write("Rank length: "+' '.join([x for x in rank_length.keys()])+'\n')
-        f.flush()
-        f.write("Rank ast: "+' '.join([x for x in rank_length.keys()])+'\n')
+        # f.write("Rank rouge: "+' '.join([x for x in rank_rouge.keys()])+'\n')
+        # f.flush()
+        # f.write("Rank length: "+' '.join([x for x in rank_length.keys()])+'\n')
+        # f.flush()
+        f.write("Rank ast: "+' '.join([x for x in rank_ast.keys()])+'\n')
         f.flush()
         ranking={}
         ranking_new={}
         for idx,k in enumerate(rank_code):
             ranking[k]=idx+1
             ranking_new[k]=idx+1
-        for idx,k in enumerate(rank_rouge):
-            ranking[k]=ranking[k]+idx+1
-            ranking_new[k]=ranking_new[k]+idx+1
-        for idx,k in enumerate(rank_length):
-            ranking[k]=ranking[k]+idx+1
-            ranking_new[k]=ranking_new[k]+idx+1
+        # for idx,k in enumerate(rank_rouge):
+        #     ranking[k]=ranking[k]+idx+1
+        #     ranking_new[k]=ranking_new[k]+idx+1
+        # for idx,k in enumerate(rank_length):
+        #     ranking[k]=ranking[k]+idx+1
+        #    ranking_new[k]=ranking_new[k]+idx+1
         for idx,k in enumerate(rank_ast):
             ranking_new[k]=ranking_new[k]+idx+1
         for key in ranking.keys():
-            ranking[key]=float(ranking[key])/3
+            ranking[key]=float(ranking[key])/1
         for key in ranking_new.keys():
-            ranking_new[key]=float(ranking_new[key])/4
+            ranking_new[key]=float(ranking_new[key])/2
         ranking=dict(sorted(ranking.items(), key=lambda item: item[1]))
         ranking_new=dict(sorted(ranking_new.items(), key=lambda item: item[1]))
         top_4_result=[]
@@ -138,104 +138,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
     test_projects=args.project
     num_core=args.core
-    if(len(test_projects)==1):
-        q=multiprocessing.Queue()
-        project=test_projects[0]
+    list_process=[]        
+    for i in range(len(test_projects)):
+        project=test_projects[i]
         projects=original_projects.copy()
         projects.remove(project)
-        rank_code={}
-        rank_rouge={}
-        rank_length={}
-        rank_ast={}
-        with open(f'../dataset_v2/original/{project}/all_truncated_final.code',"r") as f1:
-            data_original=f1.readlines()
-        original_code_semantic=torch.load(os.path.join(dataset_dir,f'{project}/all_code_semantic.pt'), map_location='cpu')
-        original_code_semantic=torch.max(original_code_semantic, 0).values
-        original_ast_semantic=torch.load(os.path.join(dataset_dir,f'{project}/all_ast_semantic.pt'), map_location='cpu')
-        original_ast_semantic=torch.max(original_ast_semantic, 0).values
-        #print(projects)
-        for target_pro in projects:
-            target_semantic=torch.load(os.path.join(dataset_dir,f'{target_pro}/all_code_semantic.pt'), map_location='cpu')
-            target_semantic=torch.max(target_semantic, 0).values
-            target_ast_semantic=torch.load(os.path.join(dataset_dir,f'{target_pro}/all_ast_semantic.pt'), map_location='cpu')
-            target_ast_semantic=torch.max(target_ast_semantic, 0).values
-            rank_code[target_pro]=torch.cosine_similarity(original_code_semantic,target_semantic,-1).item()
-            rank_ast[target_pro]=torch.cosine_similarity(original_ast_semantic,target_ast_semantic,-1).item()
-            rank_length[target_pro]=abs(get_total_length(data_original)-get_total_length(data_target))
-        #print(rank_code)
-        for idx,target_pro in enumerate(projects):  
-            with open(f'../dataset_v2/original/{target_pro}/all_truncated_final.code',"r") as f2:
-                data_target=f2.readlines()
-            rank_rouge[target_pro]=get_rouge(data_original,data_target)
-        rank_code=dict(sorted(rank_code.items(), key=lambda item: item[1],reverse=True))
-        rank_ast=dict(sorted(rank_ast.items(), key=lambda item: item[1],reverse=True))
-        rank_rouge=dict(sorted(rank_rouge.items(), key=lambda item: item[1],reverse=True))
-        rank_length=dict(sorted(rank_length.items(), key=lambda item: item[1]))
-        with open(f'../dataset_v2/original/{project}/result_meta_dataset.txt',"w") as f:
-            f.write(project+'\n')
-            f.flush()
-            print(project, flush=True)
-            print("Rank code: ",' '.join([x for x in rank_code.keys()]), flush=True)
-            print("Rank rouge: ",' '.join([x for x in rank_rouge.keys()]), flush=True)
-            print("Rank length: ",' '.join([x for x in rank_length.keys()]), flush=True)
-            print("Rank ast: ",' '.join([x for x in rank_ast.keys()]), flush=True)
-            f.write("Rank code: "+' '.join([x for x in rank_code.keys()])+'\n')
-            f.flush()
-            f.write("Rank rouge: "+' '.join([x for x in rank_rouge.keys()])+'\n')
-            f.flush()
-            f.write("Rank length: "+' '.join([x for x in rank_length.keys()])+'\n')
-            f.flush()
-            f.write("Rank ast: "+' '.join([x for x in rank_length.keys()])+'\n')
-            f.flush()
-            ranking={}
-            ranking_new={}
-            for idx,k in enumerate(rank_code):
-                ranking[k]=idx+1
-                ranking_new[k]=idx+1
-            for idx,k in enumerate(rank_rouge):
-                ranking[k]=ranking[k]+idx+1
-                ranking_new[k]=ranking_new[k]+idx+1
-            for idx,k in enumerate(rank_length):
-                ranking[k]=ranking[k]+idx+1
-                ranking_new[k]=ranking_new[k]+idx+1
-            for idx,k in enumerate(rank_ast):
-                ranking_new[k]=ranking_new[k]+idx+1
-            for key in ranking.keys():
-                ranking[key]=float(ranking[key])/3
-            for key in ranking_new.keys():
-                ranking_new[key]=float(ranking_new[key])/4
-            ranking=dict(sorted(ranking.items(), key=lambda item: item[1]))
-            ranking_new=dict(sorted(ranking_new.items(), key=lambda item: item[1]))
-            top_4_result=[]
-            top_4_result_new=[]
-            for idx,key in enumerate(ranking):
-                top_4_result.append(key)
-                if idx==3:
-                    break
-            for idx,key in enumerate(ranking_new):
-                top_4_result_new.append(key)
-                if idx==3:
-                    break
-            print("Top 4 (not include ast): ",' '.join([x for x in top_4_result]), flush=True)
-            f.write("Top 4 (not include ast): "+' '.join([x for x in top_4_result])+'\n')
-            f.flush()
-            print("Top 4: ",' '.join([x for x in top_4_result_new]), flush=True)
-            f.write("Top 4: "+' '.join([x for x in top_4_result_new])+'\n')
-            f.flush()
-    else:  
-        list_process=[]        
-        for i in range(len(test_projects)):
-            project=test_projects[i]
-            projects=original_projects.copy()
-            projects.remove(project)
-            p=multiprocessing.Process(target=get_result,args=(project,projects))
-            print(f"Run process {i}", flush=True)
-            p.start()
-            list_process.append(p)
+        p=multiprocessing.Process(target=get_result,args=(project,projects))
+        print(f"Run process {i}", flush=True)
+        p.start()
+        list_process.append(p)
 
-        for i in range(len(test_projects)):
-            list_process[i].join() 
-            print(f"Finish process {i}", flush=True)   
+    for i in range(len(test_projects)):
+        list_process[i].join() 
+        print(f"Finish process {i}", flush=True)   
     # with open(f'../dataset_v2/original/result_meta_dataset.txt',"w") as f:
     #     for project in test_projects:
     #         projects=original_projects.copy()
---
+++ main-meta-new.py
@@ -0,0 +1,153 @@
+import os
+import argparse
+import config
+import metatrain_3 as metatrain
+import eval
+import random
+import train
+import torch
+torch.manual_seed(1)
+def _train(training_projects,validating_project,lr,vocab_file_path=None, model_file_path=None):
+    print('\nStarting the training process......\n')
+
+    if vocab_file_path:
+        code_vocab_path, ast_vocab_path, nl_vocab_path = vocab_file_path
+        print('Vocabulary will be built by given file path.')
+        print('\tsource code vocabulary path:\t', os.path.join(config.vocab_dir, code_vocab_path))
+        print('\tast of code vocabulary path:\t', os.path.join(config.vocab_dir, ast_vocab_path))
+        print('\tcode comment vocabulary path:\t', os.path.join(config.vocab_dir, nl_vocab_path))
+    else:
+        print('Vocabulary will be built according to dataset.')
+
+    if model_file_path:
+        print('Model will be built by given state dict file path:', os.path.join(config.model_dir, model_file_path))
+    else:
+        print('Model will be created by program.')
+
+    print('\nInitializing the training environments......\n')
+    train_instance = metatrain.MetaTrain(training_projects=training_projects,validating_project=validating_project,vocab_file_path=vocab_file_path, model_file_path=model_file_path,lr=lr)
+    print('Environments built successfully.\n')
+    print('Size of train dataset:', train_instance.meta_datasets_size)
+
+    config.logger.info('Size of train dataset:{}'.format(train_instance.meta_datasets_size))
+    config.logger.info('Size of source code vocabulary: {}'.format(train_instance.code_vocab_size))
+    config.logger.info('Size of ast of code vocabulary: {}'.format(train_instance.ast_vocab_size))
+    config.logger.info('Size of code comment vocabulary: {} '.format(train_instance.nl_vocab_size))
+
+    if config.validate_during_train:
+        print('\nValidate every', config.validate_every, 'batches and each epoch.')
+        #print('Size of validation dataset:', train_instance.eval_instance.dataset_size)
+        #config.logger.info('Size of validation dataset: {}'.format(train_instance.eval_instance.dataset_size))
+
+    print('\nStart training......\n')
+    config.logger.info('Start training.')
+    best_model = train_instance.run_train()
+    print('\nTraining is done.')
+    config.logger.info('Training is done.')
+
+    # writer = SummaryWriter('runs/CodePtr')
+    # for _, batch in enumerate(train_instance.train_dataloader):
+    #     batch_size = len(batch[0][0])
+    #     writer.add_graph(train_instance.model, (batch, batch_size, train_instance.nl_vocab))
+    #     break
+    # writer.close()
+    del train_instance
+    torch.cuda.empty_cache()
+    return best_model
+
+
+def _test(model,vocab_file_path,testing_project,num_of_data=-1):
+    dataset_dir = "../dataset_v2/"
+    if isinstance(model, dict):
+        train_instance = train.Train(vocab_file_path=vocab_file_path, model_state_dict=model,
+                                    code_path=os.path.join(dataset_dir,f'original/{testing_project}/train.code')
+                                    ,ast_path=os.path.join(dataset_dir,f'original/{testing_project}/train.sbt'),
+                                    nl_path=os.path.join(dataset_dir,f'original/{testing_project}/train.comment'),batch_size=config.support_batch_size,
+                                    code_valid_path=f'../dataset_v2/original/{testing_project}/valid_transfer.code',nl_valid_path=f'../dataset_v2/original/{testing_project}/valid_transfer.comment',
+                                        ast_valid_path=f'../dataset_v2/original/{testing_project}/valid_transfer.sbt'
+                                        ,num_of_data=num_of_data)
+    elif isinstance(model,str):
+        train_instance = train.Train(vocab_file_path=vocab_file_path, model_file_path=model,
+                                    code_path=os.path.join(dataset_dir,f'original/{testing_project}/train.code')
+                                    ,ast_path=os.path.join(dataset_dir,f'original/{testing_project}/train.sbt'),
+                                    nl_path=os.path.join(dataset_dir,f'original/{testing_project}/train.comment'),batch_size=config.support_batch_size,
+                                    code_valid_path=f'../dataset_v2/original/{testing_project}/valid_transfer.code',nl_valid_path=f'../dataset_v2/original/{testing_project}/valid_transfer.comment',
+                                        ast_valid_path=f'../dataset_v2/original/{testing_project}/valid_transfer.sbt'
+                                        ,num_of_data=num_of_data)       
+    best_model_test_dict=train_instance.run_train()
+    print('\nInitializing the test environments......')
+    test_instance = eval.Test(best_model_test_dict,
+                              code_path=os.path.join(dataset_dir,f'original/{testing_project}/valid.code')
+                                ,ast_path=os.path.join(dataset_dir,f'original/{testing_project}/valid.sbt'),
+                                nl_path=os.path.join(dataset_dir,f'original/{testing_project}/valid.comment'))
+    print('Environments built successfully.\n')
+    config.logger.info('Size of test dataset: {}'.format(test_instance.dataset_size))
+
+    config.logger.info('Start Testing.')
+    print('\nStart Testing......')
+    test_instance.run_test()
+    print('Testing is done.')
+
+
+def split_dataset(projects):
+    random.seed(1)
+
+    testing_project=random.choice(projects)
+    projects.remove(testing_project)
+    validating_project=random.choice(projects)
+    projects.remove(validating_project)
+    training_projects = projects
+
+    return (training_projects, validating_project, testing_project)
+
+def list_of_strings(arg):
+    return arg.split(',')
+
+if __name__ == '__main__':
+    # projects = ['AppScale/appscale','edx/edx-platform','sympy/sympy','IronLanguages/main','mne-tools/mne-python','JiYou/openstack','openhatch/oh-mainline','cloudera/hue','ahmetcemturan/SFACT','mne-tools/mne-python'] # tạm fix cứng
+    # training_projects=['AppScale/appscale','edx/edx-platform','sympy/sympy' ,'JiYou/openstack','IronLanguages/main','openhatch/oh-mainline','mne-tools/mne-python','cloudera/hue']
+    # validating_project="ahmetcemturan/SFACT"
+    # testing_project="kbengine/kbengine"
+    project2sources = {
+        'spring-boot': ['spring-framework', 'dubbo', 'spring-security'], 
+        'spring-framework': ['spring-boot', 'spring-security', 'dubbo'], 
+        'spring-security': ['spring-framework', 'spring-boot', 'dubbo'], 
+        'guava': ['flink', 'dubbo', 'kafka'], 
+        'ExoPlayer': ['flink', 'guava', 'spring-framework'], 
+        'dagger': ['dubbo', 'guava', 'spring-framework'], 
+        'kafka': ['flink', 'dubbo', 'spring-framework'], 
+        'dubbo': ['flink', 'spring-framework', 'kafka'], 
+        'flink': ['kafka', 'dubbo', 'spring-framework'], 
+    }
+    project2validate={        
+        'spring-boot': 'flink', 
+        'spring-framework': 'flink', 
+        'spring-security': 'flink', 
+        'guava': 'spring-framework', 
+        'ExoPlayer': 'kafka', 
+        'dagger': 'flink', 
+        'kafka': 'guava', 
+        'dubbo': 'guava', 
+        'flink': 'guava', }
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-v', '--validate', type=str,default=None)
+
+    parser.add_argument('-t','--test',
+                        type=list_of_strings)
+    parser.add_argument('-lr','--learning',type=float,default=0.05)
+    args = parser.parse_args()
+    # validating_project='dubbo'
+    # testing_project='dagger'
+    #training_projects=['dubbo','guava','kafka']
+    testing_project=args.test
+        #training_projects, validating_project, testing_project = split_dataset(projects)
+    for project in testing_project:
+        config.logger.info(f'validate: {project2validate[project]}, testing: {project}')
+        best_model_dict = _train(training_projects=project2sources[project], \
+                                validating_project=project2validate[project],\
+                                vocab_file_path=(config.code_vocab_path, config.ast_vocab_path, config.nl_vocab_path)
+                                ,model_file_path='../pretrain_model/pretrain.pt',lr=args.learning)
+    #_test(best_model_dict,vocab_file_path=(config.code_vocab_path, config.ast_vocab_path, config.nl_vocab_path),testing_project=testing_project,num_of_data=100)
+    
+    #  _test(os.path.join('20240511_132257', 'model_valid-loss-3.3848_epoch-14_batch--1.pt'))
+
